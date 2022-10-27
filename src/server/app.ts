8e5e333e3462870f6fd953e934dac2ce2432f2b7
정민 <jjminn0@naver.com>
2022-10-27 14:33:47 +0900

    Update : reportRouter 등록

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,15 +1,16 @@
-import express from "express"
-import {router} from "./routers/router"
+import express  from "express"
+import {reportRouter} from "./routers/reportRouter"
+
+const path = require("path")
 const app = express();
 
 app.use(express.static("public"));
-//app.use('/js', express.static('public/js'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //라우터 등록
-app.use("/api",router)
+app.use("/api", reportRouter)
 
 
 app.listen(3000, ()=>{
